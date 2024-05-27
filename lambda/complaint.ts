e11a7fd72abbf5dbbe5c7b9ce73822da8db27ddf
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-05-28 00:30:08 +0530

    Updated complaint routes

---
+++ lambda/complaint.ts
@@ -0,0 +1,40 @@
+import { Hono } from "hono";
+import { ComplaintController } from "../controller/complaint";
+import {
+  authenticate,
+  authenticateEmployee,
+} from "../middleware/auth.middleware";
+import { uploadMiddleware } from "../middleware/upload.middleware";
+
+const complaintRoutes = new Hono();
+const complaintController = new ComplaintController();
+
+complaintRoutes.post(
+  "/",
+  authenticate,
+  uploadMiddleware,
+  complaintController.createComplaint
+);
+complaintRoutes.get(
+  "/",
+  authenticateEmployee,
+  complaintController.getAllComplaints
+);
+complaintRoutes.get("/:id", complaintController.getComplaintByID);
+complaintRoutes.get(
+  "/user",
+  authenticate,
+  complaintController.getComplaintsByUserID
+);
+complaintRoutes.post(
+  "/note",
+  authenticateEmployee,
+  complaintController.createComplaintNote
+);
+complaintRoutes.get(
+  "/note/:complaintID",
+  authenticateEmployee,
+  complaintController.getComplaintNotes
+);
+
+export default complaintRoutes;

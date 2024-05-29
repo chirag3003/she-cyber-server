5fd4c883be0ad7978f1b8f715318fafa747611b0
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-05-30 02:04:55 +0530

    employee service

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,162 +1,166 @@
-import { Context } from "hono";
+import {Context} from "hono";
 import {
-  createUserValidator,
-  loginUserValidator,
-  responseUserValidator,
+    createUserValidator,
+    loginUserValidator,
+    responseUserValidator,
 } from "../validators/user.validator";
-import { ZodError } from "zod";
-import { StatusCodes } from "http-status-codes";
-import { AuthService } from "../service/auth.service";
+import {ZodError} from "zod";
+import {StatusCodes} from "http-status-codes";
+import {AuthService} from "../service/auth.service";
 import {
-  generateEmployeeJWT,
-  generateJWT,
-  getPasswordKeys,
-  validatePassword,
+    generateEmployeeJWT,
+    generateJWT,
+    getPasswordKeys,
+    validatePassword,
 } from "../lib/auth.lib";
-import { UserService } from "../service/user.service";
+import {UserService} from "../service/user.service";
 import {
-  createEmployeeValidator,
-  loginEmployeeValidator,
+    createEmployeeValidator,
+    loginEmployeeValidator,
 } from "../validators/employee.validator";
-import { EmployeeService } from "../service/employee.service";
+import {EmployeeService} from "../service/employee.service";
 
 const authService = new AuthService();
 const userService = new UserService();
 const employeeService = new EmployeeService();
 
 export class AuthController {
-  async passwordLogin(ctx: Context): Promise<Response> {
-    try {
-      const body = loginUserValidator.parse(await ctx.req.json());
-      let user = await userService.findUserByPhoneNo(body.identity);
-      if (!user) {
-        user = await userService.findUserByEmail(body.identity);
-      }
-      if (!user) {
-        return ctx.json({ message: "User not found" }, StatusCodes.NOT_FOUND);
-      }
-      const passwordValid = validatePassword(
-        body.password,
-        user.hash,
-        user.salt
-      );
-      if (!passwordValid) {
-        return ctx.json(
-          { message: "Invalid password" },
-          StatusCodes.UNAUTHORIZED
-        );
-      }
-      const token = await generateJWT({ id: user.id });
-      return ctx.json({ token, user: responseUserValidator.parse(user) });
-    } catch (err) {
-      if (err instanceof ZodError) {
-        return ctx.json({ message: err.errors }, StatusCodes.BAD_REQUEST);
-      }
-      return ctx.json(
-        { message: "Internal Server Error" },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async passwordLogin(ctx: Context): Promise<Response> {
+        try {
+            const body = loginUserValidator.parse(await ctx.req.json());
+            let user = await userService.findUserByPhoneNo(body.identity);
+            if (!user) {
+                user = await userService.findUserByEmail(body.identity);
+            }
+            if (!user) {
+                return ctx.json({message: "User not found"}, StatusCodes.NOT_FOUND);
+            }
+            const passwordValid = validatePassword(
+                body.password,
+                user.hash,
+                user.salt
+            );
+            if (!passwordValid) {
+                return ctx.json(
+                    {message: "Invalid password"},
+                    StatusCodes.UNAUTHORIZED
+                );
+            }
+            const token = await generateJWT({id: user.id});
+            return ctx.json({token, user: responseUserValidator.parse(user)});
+        } catch (err) {
+            if (err instanceof ZodError) {
+                return ctx.json({message: err.errors}, StatusCodes.BAD_REQUEST);
+            }
+            return ctx.json(
+                {message: "Internal Server Error"},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
-  async otpLogin(ctx: Context): Promise<Response> {
-    try {
-      const body = loginUserValidator.parse(await ctx.req.json());
-      let user = await userService.findUserByPhoneNo(body.identity);
-      if (!user) {
-        return ctx.json({ message: "User not found" }, StatusCodes.NOT_FOUND);
-      }
-      const token = await generateJWT({ id: user.id });
-      return ctx.json({ token, user: responseUserValidator.parse(user) });
-    } catch (err) {
-      if (err instanceof ZodError) {
-        return ctx.json({ message: err.errors }, StatusCodes.BAD_REQUEST);
-      }
-      console.error(err);
-      return ctx.json(
-        { message: "Internal Server Error" },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
-    }
-  }
 
-  async signup(ctx: Context): Promise<Response> {
-    try {
-      const body = createUserValidator.parse(await ctx.req.json());
-      const { hash, salt } = getPasswordKeys(body.password);
-      await authService.createUser(body, hash, salt);
-      const user = await userService.findUserByPhoneNo(body.phoneNo);
-      const token = await generateJWT({ id: user!.id });
-      return ctx.json(
-        {
-          token,
-          user: responseUserValidator.parse(user),
-        },
-        StatusCodes.CREATED
-      );
-    } catch (e) {
-      if (e instanceof ZodError) {
-        return ctx.json({ message: e.errors }, StatusCodes.BAD_REQUEST);
-      }
-      return ctx.json(
-        { message: "Internal Server Error" },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async otpLogin(ctx: Context): Promise<Response> {
+        try {
+            const body = loginUserValidator.parse(await ctx.req.json());
+            let user = await userService.findUserByPhoneNo(body.identity);
+            if (!user) {
+                return ctx.json({message: "User not found"}, StatusCodes.NOT_FOUND);
+            }
+            const token = await generateJWT({id: user.id});
+            return ctx.json({token, user: responseUserValidator.parse(user)});
+        } catch (err) {
+            if (err instanceof ZodError) {
+                return ctx.json({message: err.errors}, StatusCodes.BAD_REQUEST);
+            }
+            console.error(err);
+            return ctx.json(
+                {message: "Internal Server Error"},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
-  async createEmployee(ctx: Context): Promise<Response> {
-    try {
-      const body = createEmployeeValidator.parse(await ctx.req.parseBody());
-      const { hash, salt } = getPasswordKeys(body.password);
-      await authService.createEmployee(body, hash, salt);
-      const user = await employeeService.getEmployeeByPhone(body.phoneNo);
-      const token = await generateEmployeeJWT({ id: user!.id });
-      return ctx.json(
-        {
-          token,
-          user: responseUserValidator.parse(user),
-        },
-        StatusCodes.CREATED
-      );
-    } catch (e) {
-      if (e instanceof ZodError) {
-        return ctx.json({ message: e.errors }, StatusCodes.BAD_REQUEST);
-      }
-      console.error(e);
-      return ctx.json(
-        { message: "Internal Server Error" },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+
+    async signup(ctx: Context): Promise<Response> {
+        try {
+            const body = createUserValidator.parse(await ctx.req.json());
+            const {hash, salt} = getPasswordKeys(body.password);
+            await authService.createUser(body, hash, salt);
+            const user = await userService.findUserByPhoneNo(body.phoneNo);
+            const token = await generateJWT({id: user!.id});
+            return ctx.json(
+                {
+                    token,
+                    user: responseUserValidator.parse(user),
+                },
+                StatusCodes.CREATED
+            );
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return ctx.json({message: e.errors}, StatusCodes.BAD_REQUEST);
+            }
+            return ctx.json(
+                {message: "Internal Server Error"},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
-  async employeeLogin(ctx: Context): Promise<Response> {
-    try {
-      const body = loginEmployeeValidator.parse(await ctx.req.json());
-      let user = await employeeService.getEmployeeByPhone(body.phoneNo);
-      if (!user) {
-        return ctx.json({ message: "User not found" }, StatusCodes.NOT_FOUND);
-      }
-      const passwordValid = validatePassword(
-        body.password,
-        user.hash,
-        user.salt
-      );
-      if (!passwordValid) {
-        return ctx.json(
-          { message: "Invalid password" },
-          StatusCodes.UNAUTHORIZED
-        );
-      }
-      const token = await generateEmployeeJWT({ id: user.id });
-      return ctx.json({ token, user: responseUserValidator.parse(user) });
-    } catch (err) {
-      if (err instanceof ZodError) {
-        return ctx.json({ message: err.errors }, StatusCodes.BAD_REQUEST);
-      }
-      console.error(err);
-      return ctx.json(
-        { message: "Internal Server Error" },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+
+    async createEmployee(ctx: Context): Promise<Response> {
+        try {
+            const body = createEmployeeValidator.parse(await ctx.req.parseBody());
+            const {hash, salt} = getPasswordKeys(body.password);
+            await authService.createEmployee(body, hash, salt);
+            const user = await employeeService.getEmployeeByPhone(body.phoneNo);
+            const token = await generateEmployeeJWT({id: user!.id});
+            return ctx.json(
+                {
+                    token,
+                    user: responseUserValidator.parse(user),
+                    admin: false
+                },
+                StatusCodes.CREATED
+            );
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return ctx.json({message: e.errors}, StatusCodes.BAD_REQUEST);
+            }
+            console.error(e);
+            return ctx.json(
+                {message: "Internal Server Error"},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
+
+    async employeeLogin(ctx: Context): Promise<Response> {
+        try {
+            const body = loginEmployeeValidator.parse(await ctx.req.json());
+            let user = await employeeService.getEmployeeByPhone(body.phoneNo);
+            if (!user) {
+                return ctx.json({message: "User not found"}, StatusCodes.NOT_FOUND);
+            }
+            const passwordValid = validatePassword(
+                body.password,
+                user.hash,
+                user.salt
+            );
+            if (!passwordValid) {
+                return ctx.json(
+                    {message: "Invalid password"},
+                    StatusCodes.UNAUTHORIZED
+                );
+            }
+            const token = await generateEmployeeJWT({id: user.id});
+            return ctx.json({token, user: responseUserValidator.parse(user), admin: false});
+        } catch (err) {
+            if (err instanceof ZodError) {
+                return ctx.json({message: err.errors}, StatusCodes.BAD_REQUEST);
+            }
+            console.error(err);
+            return ctx.json(
+                {message: "Internal Server Error"},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }

5209e12d403d562c6be4d46cbe7c56546d0aed19
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-05-31 22:01:03 +0530

    added admin only middleware

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
 import {Context, Next} from "hono";
 import {STATUS_CODES} from "node:http";
-import {StatusCodes} from "http-status-codes";
+import {ReasonPhrases, StatusCodes} from "http-status-codes";
 import {verifyEmployeeJWT, verifyJWT} from "../lib/auth.lib";
 import {EmployeeService} from "../service/employee.service";
 
@@ -56,3 +56,21 @@ export const authenticateEmployee = async (ctx: Context, next: Next) => {
         );
     }
 };
+export const adminOnly = async (ctx: Context, next: Next) => {
+    try {
+        const isAdmin = ctx.get("isAdmin") as boolean;
+        if (!isAdmin) {
+            return ctx.json(
+                {
+                    message: "You are not authorized to access this resource",
+                },
+                StatusCodes.FORBIDDEN
+            );
+        }
+        await next();
+        return;
+    } catch (e) {
+        console.error(e)
+        return ctx.json({message: ReasonPhrases.INTERNAL_SERVER_ERROR}, StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}

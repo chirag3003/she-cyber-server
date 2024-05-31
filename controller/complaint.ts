43c8c95a228dd96ce9eae36b1927aa3e82d00b26
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-05-31 22:46:50 +0530

    complaints

--- controller/complaint.ts
+++ controller/complaint.ts
@@ -1,109 +1,110 @@
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { ComplaintService } from "../service/complaint.service";
-import { UserService } from "../service/user.service";
+import {ReasonPhrases, StatusCodes} from "http-status-codes";
+import {ComplaintService} from "../service/complaint.service";
+import {UserService} from "../service/user.service";
 import {
-  createComplaintNoteValidator,
-  createComplaintValidator,
+    createComplaintNoteValidator,
+    createComplaintValidator,
 } from "../validators/complaint.validator";
-import { Context } from "hono";
-import { ZodError } from "zod";
+import {Context} from "hono";
+import {ZodError} from "zod";
 
 const complaintService = new ComplaintService();
 const userService = new UserService();
 
 export class ComplaintController {
-  async createComplaint(ctx: Context): Promise<Response> {
-    try {
-      const userID = ctx.get("userID") as string;
-      const body = await ctx.req.parseBody();
-      body.attachments = ctx.get("files");
-      const input = createComplaintValidator.parse(body);
-      await complaintService.createComplaint(input, userID);
-      return ctx.json(
-        { message: "Complaint created successfully" },
-        StatusCodes.CREATED
-      );
-    } catch (e) {
-      if (e instanceof ZodError) {
-        return ctx.json({ message: e.errors }, StatusCodes.BAD_REQUEST);
-      }
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async createComplaint(ctx: Context): Promise<Response> {
+        try {
+            const userID = ctx.get("userID") as string;
+            const body = await ctx.req.parseBody();
+            body.attachments = ctx.get("files");
+            const input = createComplaintValidator.parse(body);
+            await complaintService.createComplaint(input, userID);
+            return ctx.json(
+                {message: "Complaint created successfully"},
+                StatusCodes.CREATED
+            );
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return ctx.json({message: e.errors}, StatusCodes.BAD_REQUEST);
+            }
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 
-  async getComplaintsByUserID(ctx: Context): Promise<Response> {
-    try {
-      const userID = ctx.get("userID") as string;
-      const complaints = await complaintService.getComplaintsByUserID(userID);
-      return ctx.json({ data: complaints }, StatusCodes.OK);
-    } catch (e) {
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async getComplaintsByUserID(ctx: Context): Promise<Response> {
+        try {
+            const userID = ctx.get("userID") as string;
+            const complaints = await complaintService.getComplaintsByUserID(userID);
+            return ctx.json({data: complaints}, StatusCodes.OK);
+        } catch (e) {
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 
-  async getAllComplaints(ctx: Context): Promise<Response> {
-    try {
-      const complaints = await complaintService.getAllComplaints();
-      return ctx.json({ data: complaints }, StatusCodes.OK);
-    } catch (e) {
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async getAllComplaints(ctx: Context): Promise<Response> {
+        try {
+            const isAdmin = ctx.get("isAdmin") as boolean;
+            const complaints = await (isAdmin ? complaintService.getAllComplaints() : complaintService.getComplaintsByEmployeeID(ctx.get("userID")));
+            return ctx.json({data: complaints}, StatusCodes.OK);
+        } catch (e) {
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 
-  async getComplaintByID(ctx: Context): Promise<Response> {
-    try {
-      const complaintID = ctx.req.param("id");
-      const complaint = await complaintService.getComplaintByID(complaintID);
-      if (!complaint) {
-        return ctx.json(
-          { message: "Complaint not found" },
-          StatusCodes.NOT_FOUND
-        );
-      }
-      return ctx.json({ data: complaint }, StatusCodes.OK);
-    } catch (e) {
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async getComplaintByID(ctx: Context): Promise<Response> {
+        try {
+            const complaintID = ctx.req.param("id");
+            const complaint = await complaintService.getComplaintByID(complaintID);
+            if (!complaint) {
+                return ctx.json(
+                    {message: "Complaint not found"},
+                    StatusCodes.NOT_FOUND
+                );
+            }
+            return ctx.json({data: complaint}, StatusCodes.OK);
+        } catch (e) {
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 
-  async createComplaintNote(ctx: Context): Promise<Response> {
-    try {
-      const input = createComplaintNoteValidator.parse(await ctx.req.json());
-      await complaintService.createComplaintNote(input);
-      return ctx.json(
-        { message: "Note added successfully" },
-        StatusCodes.CREATED
-      );
-    } catch (e) {
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async createComplaintNote(ctx: Context): Promise<Response> {
+        try {
+            const input = createComplaintNoteValidator.parse(await ctx.req.json());
+            await complaintService.createComplaintNote(input);
+            return ctx.json(
+                {message: "Note added successfully"},
+                StatusCodes.CREATED
+            );
+        } catch (e) {
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 
-  async getComplaintNotes(ctx: Context): Promise<Response> {
-    try {
-      const complaintID = ctx.req.param("id");
-      const notes = await complaintService.getComplaintNotes(complaintID);
-      return ctx.json({ data: notes }, StatusCodes.OK);
-    } catch (e) {
-      return ctx.json(
-        { message: ReasonPhrases.INTERNAL_SERVER_ERROR },
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
+    async getComplaintNotes(ctx: Context): Promise<Response> {
+        try {
+            const complaintID = ctx.req.param("id");
+            const notes = await complaintService.getComplaintNotes(complaintID);
+            return ctx.json({data: notes}, StatusCodes.OK);
+        } catch (e) {
+            return ctx.json(
+                {message: ReasonPhrases.INTERNAL_SERVER_ERROR},
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
     }
-  }
 }
--- service/complaint.service.ts
+++ service/complaint.service.ts
@@ -1,40 +1,46 @@
-import { eq } from "drizzle-orm";
-import { db } from "../db/db";
-import { complaintTable } from "../db/schema/complaint";
-import { CreateComplaintInput, CreateComplaintNoteInput } from "../validators/complaint.validator";
-import { complaintNotesTable } from "../db/schema/complaintNotes";
+import {eq} from "drizzle-orm";
+import {db} from "../db/db";
+import {complaintTable} from "../db/schema/complaint";
+import {CreateComplaintInput, CreateComplaintNoteInput} from "../validators/complaint.validator";
+import {complaintNotesTable} from "../db/schema/complaintNotes";
 
 export class ComplaintService {
-  async createComplaint(
-    input: CreateComplaintInput,
-    userID: string
-  ): Promise<void> {
-    await db.insert(complaintTable).values({ ...input, user: userID });
-  }
+    async createComplaint(
+        input: CreateComplaintInput,
+        userID: string
+    ): Promise<void> {
+        await db.insert(complaintTable).values({...input, user: userID});
+    }
 
-  async getComplaintsByUserID(userID: string): Promise<IComplaint[]> {
-    return db.query.complaintTable.findMany({
-      where: eq(complaintTable.user, userID),
-    });
-  }
+    async getComplaintsByUserID(userID: string): Promise<IComplaint[]> {
+        return db.query.complaintTable.findMany({
+            where: eq(complaintTable.user, userID),
+        });
+    }
 
-  async getAllComplaints(): Promise<IComplaint[]> {
-    return db.query.complaintTable.findMany();
-  }
+    async getComplaintsByEmployeeID(employeeID: string): Promise<IComplaint[]> {
+        return db.query.complaintTable.findMany({
+            where: eq(complaintTable.employee, employeeID),
+        });
+    }
 
-  async getComplaintByID(id: string): Promise<IComplaint | null | undefined> {
-    return db.query.complaintTable.findFirst({
-      where: eq(complaintTable.id, id),
-    });
-  }
+    async getAllComplaints(): Promise<IComplaint[]> {
+        return db.query.complaintTable.findMany();
+    }
 
-  async createComplaintNote(input: CreateComplaintNoteInput) {
-    await db.insert(complaintNotesTable).values(input);
-  }
+    async getComplaintByID(id: string): Promise<IComplaint | null | undefined> {
+        return db.query.complaintTable.findFirst({
+            where: eq(complaintTable.id, id),
+        });
+    }
 
-  async getComplaintNotes(complaintID: string): Promise<IComplaintNote[]> {
-    return db.query.complaintNotesTable.findMany({
-      where: eq(complaintNotesTable.complaint, complaintID),
-    });
-  }
+    async createComplaintNote(input: CreateComplaintNoteInput) {
+        await db.insert(complaintNotesTable).values(input);
+    }
+
+    async getComplaintNotes(complaintID: string): Promise<IComplaintNote[]> {
+        return db.query.complaintNotesTable.findMany({
+            where: eq(complaintNotesTable.complaint, complaintID),
+        });
+    }
 }

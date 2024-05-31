54626de3f78426585f2355aa8479729afc251011
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-05-31 22:05:40 +0530

    made email and phoneNo unique for employees

---
+++ migrations/0005_outgoing_quicksilver.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "employee" ADD CONSTRAINT "employee_email_unique" UNIQUE("email");--> statement-breakpoint
+ALTER TABLE "employee" ADD CONSTRAINT "employee_phoneNo_unique" UNIQUE("phoneNo");
